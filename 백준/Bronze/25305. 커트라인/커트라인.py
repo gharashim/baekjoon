0f2f295cc1dee5f31d9273f2d5b6a61fef105ebc
gharashim <121102288+gharashim@users.noreply.github.com>
2023-02-04 11:27:32 +0900

    [Bronze II] Title: 커트라인, Time: 232 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/25305. 커트라인/커트라인.py
@@ -0,0 +1,24 @@
+def bubble_sort(L, R):
+    if L < R: return R, L
+    else: return L, R
+
+def sweep(a):
+    for i in range(len(a)-1):
+        a[i], a[i+1] = bubble_sort(a[i], a[i+1])
+    return a
+
+def check_rank(a):
+    output = True
+    for i in range(len(a)-1):
+        if a[i] < a[i+1]: output = False
+    return output
+
+N, k = map(int, input().split())
+# a = [int(input()) for _ in range(N)]
+a = list(map(int, input().split()))
+
+while(1):
+    if check_rank(a) == False: a = sweep(a)
+    else: break
+
+print(a[k-1])
